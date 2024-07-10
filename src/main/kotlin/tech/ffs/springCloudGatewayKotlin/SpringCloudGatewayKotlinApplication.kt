43430023526d0a1f8ccc84298af84b860f48c561
Fábio Siqueira <fabiofaleiros@gmail.com>
2024-07-10 13:32:52 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ src/main/kotlin/tech/ffs/springCloudGatewayKotlin/SpringCloudGatewayKotlinApplication.kt
@@ -0,0 +1,11 @@
+package tech.ffs.springCloudGatewayKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudGatewayKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCloudGatewayKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/tech/ffs/springCloudGatewayKotlin/SpringCloudGatewayKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.ffs.springCloudGatewayKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCloudGatewayKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
